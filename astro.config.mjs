3367cd788acc0ea634ea866e50df95b819949a7b
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-06-05 00:41:26 -0400

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://joserafael.pages.dev/'
+  site: 'https://jose-rafael.pages.dev/'
 })
